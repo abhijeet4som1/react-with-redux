1f8b4d17f9090ad5d5df3d32272dc301725ce262
abhijeet <abhijeet4som1@gmail.com>
2018-12-23 22:41:31 +0530

    with commit first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './client/css/index.css';
+import UserDashboard from './UserDashboard';
+
+ReactDOM.render(<UserDashboard />, document.getElementById('root'));
